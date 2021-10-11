ae641cb3e01e927ee29352fb478d92b8a21789e6
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-11 12:20:12 -0700

    Completed Part 3

---
+++ part-3/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <Person name="Vanessa" age="21" hobbies={["reading", "swimming"]} />
+            <Person name="Diogo" age="33" hobbies={["soccer", "weddings"]} />
+            <Person name="Joel" age="49" hobbies={["computer science", "teaching"]} />
+        </div>
+    )
+}
---
+++ part-3/Person.js
@@ -0,0 +1,13 @@
+function Person ({ name, age, hobbies }) {
+
+    return (
+        <div>
+            <h2>{name.length > 8 ? name.substring(0,6) : name }</h2>
+            <p>Learn some information about this person.</p>
+            <h3>{age > 18 ? "Please go vote!": "You must be 18!"}</h3>
+            <ul>
+                { hobbies.map(h => <li>{h}</li>)}
+            </ul>
+        </div>
+    )
+}
